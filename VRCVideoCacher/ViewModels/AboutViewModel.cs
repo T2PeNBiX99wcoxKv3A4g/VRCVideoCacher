9cda33d0b502ea31756b1c01e3f3b13fff5c368f
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-07-02 00:23:54 +0800

    refactor: Convert `AboutViewModel` properties to static for consistency

--- VRCVideoCacher/ViewModels/AboutViewModel.cs
+++ VRCVideoCacher/ViewModels/AboutViewModel.cs
@@ -4,6 +4,6 @@ namespace VRCVideoCacher.ViewModels;
 
 public class AboutViewModel : ViewModelBase
 {
-    public string Version => Program.Version;
-    public string CreatedBy => Localizer.Get("CreatedBy") + $" {Program.CreatorElly}, {Program.CreatorNatsumi}, {Program.CreatorHaxy}, {Program.CreatorHauskaz}, {Program.CreatorDubyaDude}";
+    public static string Version => Program.Version;
+    public static string CreatedBy => Localizer.Get("CreatedBy") + $" {Program.CreatorElly}, {Program.CreatorNatsumi}, {Program.CreatorHaxy}, {Program.CreatorHauskaz}, {Program.CreatorDubyaDude}";
 }
