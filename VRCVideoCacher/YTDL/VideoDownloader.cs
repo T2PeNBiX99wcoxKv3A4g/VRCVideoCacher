20701047ac8d8b22fbb4fa14b0973da0c744093a
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-07-19 14:24:43 +0800

    refactor: Simplify URL type handling in `VideoDownloader` using switch expression

--- VRCVideoCacher/YTDL/VideoDownloader.cs
+++ VRCVideoCacher/YTDL/VideoDownloader.cs
@@ -57,26 +57,17 @@ public class VideoDownloader
             _currentDownload = queueItem;
             OnDownloadStarted?.Invoke(queueItem);
 
-            var success = false;
+            bool success;
             try
             {
-                switch (queueItem.UrlType)
+                success = queueItem.UrlType switch
                 {
-                    case UrlType.YouTube:
-                        success = await DownloadYouTubeVideo(queueItem);
-                        break;
-                    case UrlType.PyPyDance:
-                        success = await DownloadVideoWithId(queueItem);
-                        break;
-                    case UrlType.VRDancing:
-                        success = await DownloadVRDancingVideoWithId(queueItem);
-                        break;
-                    case UrlType.Other:
-                        success = await DownloadGenericVideo(queueItem);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
+                    UrlType.YouTube => await DownloadYouTubeVideo(queueItem),
+                    UrlType.PyPyDance => await DownloadVideoWithId(queueItem),
+                    UrlType.VRDancing => await DownloadVRDancingVideoWithId(queueItem),
+                    UrlType.Other => await DownloadGenericVideo(queueItem),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
             }
             catch (Exception ex)
             {
