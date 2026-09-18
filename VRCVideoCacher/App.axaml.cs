cae1bbe8e88bfdd0ba711a1ad338eabacc49bdd9
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-19 02:00:47 +0800

    chore: enhance developer tools with auto-connect from design mode in debug builds

--- VRCVideoCacher/App.axaml.cs
+++ VRCVideoCacher/App.axaml.cs
@@ -36,7 +36,10 @@ public class App : Application
         AvaloniaXamlLoader.Load(this);
         
 #if DEBUG
-        this.AttachDeveloperTools();
+        this.AttachDeveloperTools(options =>
+        {
+            options.AutoConnectFromDesignMode = true;
+        });
 #endif
     }
 
