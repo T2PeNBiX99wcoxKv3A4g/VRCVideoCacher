bf5a9fbef310f8f9b08c4ae683af4d104af3817a
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-19 01:56:26 +0800

    chore: Add debug tools

--- VRCVideoCacher/App.axaml.cs
+++ VRCVideoCacher/App.axaml.cs
@@ -34,6 +34,10 @@ public class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        
+#if DEBUG
+        this.AttachDeveloperTools();
+#endif
     }
 
     private static async void OnMainWindowOpened(object? sender, EventArgs e)
