7f59fe0a22ba5ed38d72e21315857258a2d3fc76
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-19 13:49:50 +0800

    refactor: add `ThisType` property and annotate `ISingleton` and `Singleton` with `[PublicAPI]`

--- VRCVideoCacher/Utils/Singleton.cs
+++ VRCVideoCacher/Utils/Singleton.cs
@@ -1,15 +1,21 @@
+using JetBrains.Annotations;
 using Serilog;
 
 namespace VRCVideoCacher.Utils;
 
+[PublicAPI]
 public interface ISingleton
 {
+    Type ThisType { get; }
 }
 
+[PublicAPI]
 public abstract class Singleton<T> : ISingleton where T : Singleton<T>, new()
 {
     private static readonly Lazy<T> InstanceInternal = new(() => new(), LazyThreadSafetyMode.ExecutionAndPublication);
     protected static readonly ILogger Log = Program.Logger.ForContext<T>();
+
+    public Type ThisType { get; } = typeof(T);
 
     public static T Instance => InstanceInternal.Value;
 }
