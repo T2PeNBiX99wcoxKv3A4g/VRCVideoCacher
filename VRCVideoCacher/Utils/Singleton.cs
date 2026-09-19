b192667067cf7a8326cfede9d743f3862408dc67
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-19 14:08:17 +0800

    refactor: change `Log` in `Singleton` to instance-level for improved flexibility and clarity

--- VRCVideoCacher/Utils/Singleton.cs
+++ VRCVideoCacher/Utils/Singleton.cs
@@ -13,7 +13,7 @@ public interface ISingleton
 public abstract class Singleton<T> : ISingleton where T : Singleton<T>, new()
 {
     private static readonly Lazy<T> InstanceInternal = new(() => new(), LazyThreadSafetyMode.ExecutionAndPublication);
-    protected static readonly ILogger Log = Program.Logger.ForContext<T>();
+    protected readonly ILogger Log = Program.Logger.ForContext<T>();
 
     public Type ThisType { get; } = typeof(T);
 
