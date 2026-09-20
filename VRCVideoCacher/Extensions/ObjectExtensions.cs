00982bd99c34eb3373804c3612277acfbcac5034
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-21 01:56:00 +0800

    refactor: add async support to `Let` and `Also` extensions for improved task handling

--- VRCVideoCacher/Extensions/ObjectExtensions.cs
+++ VRCVideoCacher/Extensions/ObjectExtensions.cs
@@ -14,9 +14,19 @@ public static class ObjectExtensions
         public TResult Let<TResult>([InstantHandle] Func<T, TResult> block) => block(value);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Task<TResult> Let<TResult>([InstantHandle] Func<T, Task<TResult>> block) => block(value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T Also([InstantHandle] Action<T> block)
         {
             block(value);
+            return value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public async Task<T> Also([InstantHandle] Func<T, Task> block)
+        {
+            await block(value);
             return value;
         }
     }
