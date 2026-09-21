22623909f7f4463d3f6f97901a2035ac3f3ff8cc
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-21 21:49:22 +0800

    feat: extend `UsingUntil` with overloads for async and non-using scenarios

--- VRCVideoCacher/Utils/UsingUntil.cs
+++ VRCVideoCacher/Utils/UsingUntil.cs
@@ -11,11 +11,35 @@ public static class UsingUntil
         return new UsingUntilScope<T>(usingValue, disposeAction);
     }
 
+    public static IDisposable Run([InstantHandle] Action disposeAction) => new UsingUntilScopeWithoutUsing(disposeAction);
+
+    public static async Task<IDisposable> RunAsync([InstantHandle(RequireAwait = true)] Func<Task> usingFunc,
+        [InstantHandle] Action disposeAction)
+    {
+        await usingFunc().ConfigureAwait(false);
+        return new UsingUntilScopeWithoutUsing(disposeAction);
+    }
+
+    public static async Task<IDisposable> RunAsync<T>([InstantHandle(RequireAwait = true)] Func<Task<T>> usingFunc,
+        [InstantHandle] Action<T> disposeAction)
+    {
+        var usingValue = await usingFunc().ConfigureAwait(false);
+        return new UsingUntilScope<T>(usingValue, disposeAction);
+    }
+
     public sealed class UsingUntilScope<T>(T usingValue, Action<T> disposeAction) : IDisposable
     {
         public void Dispose()
         {
             disposeAction(usingValue);
+        }
+    }
+
+    public sealed class UsingUntilScopeWithoutUsing(Action disposeAction) : IDisposable
+    {
+        public void Dispose()
+        {
+            disposeAction();
         }
     }
 }
