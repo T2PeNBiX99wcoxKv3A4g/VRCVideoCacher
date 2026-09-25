1609520b3876b9fcc92f376a57093c94292bc93c
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-26 01:34:03 +0800

    refactor: add author null check in `YouTubeMetadataService` cache validation

--- VRCVideoCacher/Services/YouTubeMetadataService.cs
+++ VRCVideoCacher/Services/YouTubeMetadataService.cs
@@ -71,7 +71,7 @@ public static class YouTubeMetadataService
     {
         var cachedInfo = await DatabaseManager.GetVideoInfoCacheAsync(videoId);
 
-        if (videoId.Length == 11 && (cachedInfo == null || string.IsNullOrEmpty(cachedInfo.Title)))
+        if (videoId.Length == 11 && (cachedInfo == null || string.IsNullOrEmpty(cachedInfo.Title) || string.IsNullOrEmpty(cachedInfo.Author)))
             cachedInfo = await GetVideoTitleAsync(videoId);
 
         return cachedInfo;
