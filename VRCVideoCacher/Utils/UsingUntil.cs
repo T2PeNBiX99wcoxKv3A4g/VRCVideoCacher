c596c3e355353482245e8843f6c4078932d5a1b2
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-26 01:13:01 +0800

    refactor: simplify `UsingUntil` API and align with modern disposal patterns

--- VRCVideoCacher/Utils/UsingUntil.cs
+++ VRCVideoCacher/Utils/UsingUntil.cs
@@ -5,41 +5,17 @@ namespace VRCVideoCacher.Utils;
 [PublicAPI]
 public static class UsingUntil
 {
-    public static IDisposable Run<T>([InstantHandle] Func<T> usingFunc, [InstantHandle] Action<T> disposeAction)
+    public static IDisposable Run([InstantHandle] Action disposeAction) => new UsingUntilScope(disposeAction);
+
+    public static IAsyncDisposable RunAsync([InstantHandle] Func<ValueTask> disposeAction) => new UsingUntilAsyncScope(disposeAction);
+
+    public sealed class UsingUntilScope(Action disposeAction) : IDisposable
     {
-        var usingValue = usingFunc();
-        return new UsingUntilScope<T>(usingValue, disposeAction);
+        public void Dispose() => disposeAction();
     }
-
-    public static IDisposable Run([InstantHandle] Action disposeAction) => new UsingUntilScopeWithoutUsing(disposeAction);
-
-    public static async Task<IDisposable> RunAsync([InstantHandle(RequireAwait = true)] Func<Task> usingFunc,
-        [InstantHandle] Action disposeAction)
+    
+    public sealed class UsingUntilAsyncScope(Func<ValueTask> disposeAction) : IAsyncDisposable
     {
-        await usingFunc().ConfigureAwait(false);
-        return new UsingUntilScopeWithoutUsing(disposeAction);
-    }
-
-    public static async Task<IDisposable> RunAsync<T>([InstantHandle(RequireAwait = true)] Func<Task<T>> usingFunc,
-        [InstantHandle] Action<T> disposeAction)
-    {
-        var usingValue = await usingFunc().ConfigureAwait(false);
-        return new UsingUntilScope<T>(usingValue, disposeAction);
-    }
-
-    public sealed class UsingUntilScope<T>(T usingValue, Action<T> disposeAction) : IDisposable
-    {
-        public void Dispose()
-        {
-            disposeAction(usingValue);
-        }
-    }
-
-    public sealed class UsingUntilScopeWithoutUsing(Action disposeAction) : IDisposable
-    {
-        public void Dispose()
-        {
-            disposeAction();
-        }
+        public ValueTask DisposeAsync() => disposeAction();
     }
 }
