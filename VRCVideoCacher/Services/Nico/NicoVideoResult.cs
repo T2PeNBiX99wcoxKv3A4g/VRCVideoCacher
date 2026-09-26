0fb0d8ce6160865f8e08641ae6880beeecd8c7ac
ykysnk <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2026-09-26 18:35:38 +0800

    refactor: move `NicoVideoResult` to `Nico` namespace to align with folder structure

---
+++ VRCVideoCacher/Services/Nico/NicoVideoResult.cs
@@ -0,0 +1,22 @@
+using JetBrains.Annotations;
+
+namespace VRCVideoCacher.Services.Nico;
+
+[PublicAPI]
+public class NicoVideoResult
+{
+    public string? VideoId { get; set; }
+    public string? Url { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? Description { get; set; }
+    public string[]? Tags { get; set; }
+    public long? ViewCount { get; set; }
+    public long? CommentCount { get; set; }
+    public long? MyListCount { get; set; }
+    public long? LikeCount { get; set; }
+    public long? Duration { get; set; }
+    public string? Thumbnail { get; set; }
+    public string? StreamUrl { get; set; }
+    public Dictionary<string, string> Cookies { get; set; } = new();
+}
--- VRCVideoCacher/Services/NicoVideoApiService.cs
+++ VRCVideoCacher/Services/NicoVideoApiService.cs
@@ -14,25 +14,6 @@ using VRCVideoCacher.Utils;
 
 namespace VRCVideoCacher.Services;
 
-[PublicAPI]
-public class NicoVideoResult
-{
-    public string? VideoId { get; set; }
-    public string? Url { get; set; }
-    public string? Title { get; set; }
-    public string? Author { get; set; }
-    public string? Description { get; set; }
-    public string[]? Tags { get; set; }
-    public long? ViewCount { get; set; }
-    public long? CommentCount { get; set; }
-    public long? MyListCount { get; set; }
-    public long? LikeCount { get; set; }
-    public long? Duration { get; set; }
-    public string? Thumbnail { get; set; }
-    public string? StreamUrl { get; set; }
-    public Dictionary<string, string> Cookies { get; set; } = new();
-}
-
 public partial class NicoVideoApiService : Singleton<NicoVideoApiService>
 {
     public const string UserAgent =
